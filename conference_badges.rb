3c5179551cecb1667b7475a46169baf42c9dfc2f
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 20:25:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,5 @@ end
 def batch_badge_creator(array)
   array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 maybe = array.each {|name| puts "Hello, my name is #{name}"}
-maybe
+return maybe
 end
