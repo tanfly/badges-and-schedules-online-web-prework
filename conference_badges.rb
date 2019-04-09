85d0841b81a135253d0896564fe108fc9022eec2
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 20:07:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ return "Hello, my name is #{name}."
 end
 def batch_badge_creator(array)
   array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  array.each do |name| 
-    return "Hello, my name is #{name}."
+  array.each do |item| 
+    return "Hello, my name is #{item}."
   end
 end
