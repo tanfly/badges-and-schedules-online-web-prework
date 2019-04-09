f6f0def8c7d9b7bbcc5a2aa43d23992969654319
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 20:58:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,3 @@ def batch_badge_creator(attendees)
 attendees.each do |attendee| 
   array.push ("Hello, my name is #{attendee}.")
 end
-def assign_rooms(names, rooms)
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  rooms = ["1", "2", "3", "4", "5", "6", "7"]
-end  
